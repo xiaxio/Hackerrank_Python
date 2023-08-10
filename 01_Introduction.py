d5a505c76c46b55d1da62e0e61da4792d3084ae3
Aroom Gonzalez <aroom.dev@gmail.com>
2023-08-10 14:00:32 -0500

    Update 01_Introduction.py
    Chapter completed

--- 01_Introduction.py
+++ 01_Introduction.py
@@ -50,13 +50,55 @@ if __name__ == '__main__':
     print(a/b)
   
 #########################################################################################################################################################
+# Loops
+# Task
+# The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n, print i-squared.
 
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
 
 #########################################################################################################################################################
+# Write a function
+# An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+# In the Gregorian calendar, three conditions are used to identify leap years:
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+# This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.
 
+def is_leap(year):
+    leap = False
+    
+    # Verify if year is divisible by 4
+    if (year % 4) == 0:
+        leap = True
+        
+    # Check for the exception case
+    if ((year % 100) == 0 and (year % 400) != 0):
+        leap = False            
 
+    return leap
+    
 #########################################################################################################################################################
+# Print function
+# The included code stub will read an integer, n, from STDIN.
+# Without using any string methods, try to print the following:
+# 123...n
+# Note that "..." represents the consecutive values in between
 
-
+if __name__ == '__main__':
+    n = int(input())
+    m = 0
+    for i in range (1, n + 1):
+        if i < 10:
+            m = m * 10 + i
+        elif i < 100:
+            m = m * 100 + i
+        elif i < 1000:
+            m = m * 1000 + i
+    print(m)
+    
 #########################################################################################################################################################
 
