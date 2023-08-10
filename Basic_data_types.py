d4de2d5bcf8d3f49169dc27ba7bcbf5268494073
Aroom Gonzalez <aroom.dev@gmail.com>
2023-08-10 13:32:15 -0500

    Update Basic_data_types.py
    Matched the order of the problems with the website order

--- Basic_data_types.py
+++ Basic_data_types.py
@@ -1,49 +1,3 @@
-# Finding the percentage
-# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
-# Print the average of the marks array for the student name provided, showing 2 places after the decimal.
-
-if __name__ == '__main__':
-    n = int(input())
-    student_marks = {}
-    for _ in range(n):
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
-    query_name = input()
-    
-      
-    average = sum(student_marks[query_name]) / len(student_marks[query_name])
-    
-    print(f'{average:.2f}')
-
-#########################################################################################################################################################
-# Lists
-# Initialize your list and read in the value of  followed by  lines of commands where each command will be of the  types listed above. 
-# Iterate through each command in order and perform the corresponding operation on your list.
-if __name__ == '__main__':
-    N = int(input())
-    commands = list()
-    my_list = list()
-    for i in range(N):
-        command = input()
-        
-        action = command.split()[0]
-        if command == 'print':
-            print(my_list)
-        elif action == 'insert':
-            my_list.insert(int(command.split()[1]), int(command.split()[2]))
-        elif action == 'remove':
-            my_list.remove(int(command.split()[1]))
-        elif action == 'append':
-            my_list.append(int(command.split()[1]))
-        elif action == 'sort':
-            my_list.sort()
-        elif action == 'pop':
-            my_list.pop()
-        elif action == 'reverse':
-            my_list.reverse()
-
-
 #########################################################################################################################################################
 # List Comprehension
 # You are given three integers x, y and z representing the dimensions of a cuboid along with an integer n. 
@@ -85,16 +39,6 @@ if __name__ == '__main__':
 
 
 #########################################################################################################################################################
-# Tuples
-# Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers. Then compute and print the result of hash(t).
-# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
-
-if __name__ == '__main__':
-    n = int(input())
-    integer_list = map(int, input().split())
-    print(hash(tuple(integer_list)))
-
-
 # Nested lists
 # Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) 
 # having the second lowest grade.
@@ -128,5 +72,65 @@ if __name__ == '__main__':
     # Print the students names
     for i in students_with_second_lowest_grade:
         print(i)    
+
+
+#########################################################################################################################################################
+# Finding the percentage
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+# Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+      
+    average = sum(student_marks[query_name]) / len(student_marks[query_name])
+    
+    print(f'{average:.2f}')
+
+
+#########################################################################################################################################################
+# Lists
+# Initialize your list and read in the value of  followed by  lines of commands where each command will be of the  types listed above. 
+# Iterate through each command in order and perform the corresponding operation on your list.
+if __name__ == '__main__':
+    N = int(input())
+    commands = list()
+    my_list = list()
+    for i in range(N):
+        command = input()
+        
+        action = command.split()[0]
+        if command == 'print':
+            print(my_list)
+        elif action == 'insert':
+            my_list.insert(int(command.split()[1]), int(command.split()[2]))
+        elif action == 'remove':
+            my_list.remove(int(command.split()[1]))
+        elif action == 'append':
+            my_list.append(int(command.split()[1]))
+        elif action == 'sort':
+            my_list.sort()
+        elif action == 'pop':
+            my_list.pop()
+        elif action == 'reverse':
+            my_list.reverse()
+
+
+#########################################################################################################################################################
+# Tuples
+# Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers. Then compute and print the result of hash(t).
+# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    print(hash(tuple(integer_list)))
+
 
 
